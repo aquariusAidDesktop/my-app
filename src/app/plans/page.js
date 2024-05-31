793c6f9b8f5fbf04f1eb10adfb92ff3caad86130
aquariusAidDesktop <aquarius_desktop@mail.ru>
2024-05-31 11:02:50 +0300

    ertyhth

--- src/app/plans/page.js
+++ src/app/plans/page.js
@@ -1,7 +1,33 @@
+import Header from "@/components/Header/Header";
+import styles from "./page.module.css"
+
 export default function Home() {
     return (
         <>
-            <div></div>
+            <Header>
+                <div className={styles.headerContainer}>
+                        <div className={styles.header}>Планы</div>
+                    </div>
+                <div className={styles.planContainer}>
+                    <div className={styles.planDushContainer}>
+                        <div className={styles.activePlan}>
+                            <p>план 1</p>
+                        </div>
+                        <div className={styles.activePlan}>
+                            <p>план 2</p>
+                        </div>
+                        <div className={styles.activePlan}>
+                            <p>план 3</p>
+                        </div>
+                        <div className={styles.activePlan}>
+                            <p>план 4</p>
+                        </div>
+                    </div>
+                    <div className={styles.createPlan}>
+                        <p>создать план</p>
+                    </div>
+                </div>
+            </Header>
         </>
     )
 }
